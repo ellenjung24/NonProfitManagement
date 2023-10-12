3a4b7b802164e5fc55e98d7609c9c865da218383
DabeenJung <dabeenj24@gmail.com>
2023-10-12 14:33:36 -0700

    edited DonationController

--- Controllers/DonationController.cs
+++ Controllers/DonationController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -68,6 +69,9 @@ namespace NonProfitManagement.Controllers
         [Authorize (Roles = "Admin, Finance")]
         public async Task<IActionResult> Create([Bind("TransId,Date,AccountNo,TransactionTypeId,Amount,PaymentMethodId,Notes")] Donation donation)
         {
+            if (donation.Date == null) {
+                
+            }
             if (ModelState.IsValid)
             {
                 _context.Add(donation);
